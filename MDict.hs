f6679732242af4a538cfbbb74f636c3c605c4886
hashirama <noroi_harau@tutanota.com>
2025-03-12 15:08:18 -0400

    add utf8 by default and manage memory automatically

--- MDict.hs
+++ MDict.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
--- Copyright © 2025 Hashirama Senju
-
 module MDict (
   MDict,
   MDictType(..),
@@ -15,9 +13,12 @@ import Foreign
 import Foreign.C
 import Foreign.C.String
 import Foreign.C.Types
-import Foreign.ForeignPtr
+import Foreign.ForeignPtr (ForeignPtr, newForeignPtr, withForeignPtr)
 import Control.Exception (bracket, throwIO)
 import Control.Monad (when)
+import qualified Data.ByteString as BS
+import Data.Text.Encoding (decodeUtf8')
+import Data.Text (Text, unpack)
 
 -- Safe handle type
 newtype MDict = MDict (ForeignPtr ())
@@ -25,22 +26,22 @@ newtype MDict = MDict (ForeignPtr ())
 -- Dictionary type enum
 data MDictType = MDX | MDD deriving (Show, Eq)
 
--- Finalizer wrapper
-foreign import ccall "wrapper"
-  mkDestroyFinalizer :: (Ptr () -> IO ()) -> IO (FunPtr (Ptr () -> IO ()))
-
--- Foreign imports with corrected types
+-- Foreign imports
 foreign import ccall "mdict_init"
   c_mdict_init :: CString -> IO (Ptr ())
 
+-- well, that's a typo in the c++ library, but im too lazy to correct it.
 foreign import ccall "mdict_destory"
-  c_mdict_destroy :: Ptr () -> IO CInt  -- Original C returns int
+  c_mdict_destroy :: Ptr () -> IO ()
+
+foreign import ccall "&mdict_destory"
+  c_mdict_destroy_finalizer :: FunPtr (Ptr () -> IO ())
 
 foreign import ccall "mdict_lookup"
   c_mdict_lookup :: Ptr () -> CString -> Ptr (Ptr CChar) -> IO ()
 
 foreign import ccall "mdict_keylist"
-  c_mdict_keylist :: Ptr () -> Ptr CULong -> IO (Ptr (Ptr CChar))  -- Changed to Ptr CChar
+  c_mdict_keylist :: Ptr () -> Ptr CULong -> IO (Ptr (Ptr CChar))
 
 foreign import ccall "free_simple_key_list"
   c_free_simple_key_list :: Ptr (Ptr CChar) -> CULong -> IO ()
@@ -51,6 +52,14 @@ foreign import ccall "mdict_suggest"
 foreign import ccall "mdict_filetype"
   c_mdict_filetype :: Ptr () -> IO CInt
 
+-- Custom UTF-8 string decoder
+peekUtf8CString :: CString -> IO String
+peekUtf8CString cstr = do
+  bs <- BS.packCString cstr
+  case decodeUtf8' bs of
+    Left _ -> return ""
+    Right text -> return $ unpack text
+
 -- Safe initialization/destruction
 withMDict :: FilePath -> (MDict -> IO a) -> IO a
 withMDict path action = 
@@ -59,12 +68,10 @@ withMDict path action =
       (do rawPtr <- c_mdict_init cpath
           when (rawPtr == nullPtr) $ 
             throwIO (userError "Failed to initialize MDict")
-          finalizer <- mkDestroyFinalizer $ \ptr -> do
-            _ <- c_mdict_destroy ptr  -- Discard return code
-            return ()
-          newForeignPtr finalizer rawPtr)
-      (\fptr -> return ())
-      (action . MDict)
+          fptr <- newForeignPtr c_mdict_destroy_finalizer rawPtr
+          return (MDict fptr))
+      (\_ -> return ())
+      action
 
 -- Safe lookup function
 lookupWord :: MDict -> String -> IO (Maybe String)
@@ -76,7 +83,7 @@ lookupWord (MDict fptr) word =
         result <- peek resultPtr
         if result == nullPtr
           then return Nothing
-          else Just <$> peekCString result
+          else Just <$> peekUtf8CString result
 
 -- Key list handling
 getKeys :: MDict -> IO [String]
@@ -89,7 +96,7 @@ getKeys (MDict fptr) =
         then return []
         else do
           keyPtrs <- peekArray (fromIntegral len) keysPtr
-          keys <- mapM peekCString keyPtrs
+          keys <- mapM peekUtf8CString keyPtrs
           c_free_simple_key_list keysPtr (fromIntegral len)
           return keys
 
@@ -105,4 +112,4 @@ suggestWords (MDict fptr) word maxSuggestions =
           then return []
           else do
             suggestions <- peekArray maxSuggestions results
-            mapM peekCString suggestions
+            mapM peekUtf8CString suggestions
